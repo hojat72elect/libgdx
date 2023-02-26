2b5e25113e047e1303aa9822fb0870eeeb563e75
hojat <hojat72elect@gmail.com>
2023-02-26 03:03:35 -0500

    Cleaned Hiero

--- extensions/gdx-tools/src/com/badlogic/gdx/tools/hiero/unicodefont/effects/ConfigurableEffect.java
+++ extensions/gdx-tools/src/com/badlogic/gdx/tools/hiero/unicodefont/effects/ConfigurableEffect.java
@@ -13,40 +13,40 @@ public interface ConfigurableEffect extends Effect {
      * Returns the list of {@link Value}s for this effect. This list is not typically backed by the effect, so changes to the
      * values will not take affect until {@link #setValues(List)} is called.
      */
-    public List getValues();
+    List getValues();
 
     /**
      * Sets the list of {@link Value}s for this effect.
      */
-    public void setValues(List values);
+    void setValues(List values);
 
     /**
      * Represents a configurable value for an effect.
      */
-    static public interface Value {
+    interface Value {
         /**
          * Returns the name of the value.
          */
-        public String getName();
+        String getName();
 
         /**
          * Sets the string representation of the value.
          */
-        public void setString(String value);
+        void setString(String value);
 
         /**
          * Gets the string representation of the value.
          */
-        public String getString();
+        String getString();
 
         /**
          * Gets the object representation of the value.
          */
-        public Object getObject();
+        Object getObject();
 
         /**
          * Shows a dialog allowing a user to configure this value.
          */
-        public void showDialog();
+        void showDialog();
     }
 }
