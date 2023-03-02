f938c64bfd192b6564085264a07199ca8b69ec6d
hojat <hojat72elect@gmail.com>
2023-03-02 00:44:16 -0500

    new tests for Rectangle.java

--- gdx/test/com/badlogic/gdx/math/RectangleTest.kt
+++ gdx/test/com/badlogic/gdx/math/RectangleTest.kt
@@ -4,20 +4,49 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class RectangleTest {
+    private val mockRectangleString = "[5.0,-4.1,0.03,-0.02]"
+    private val mockRectangle = Rectangle(5f, -4.1f, 0.03f, -0.02f)
+    private val defaultRectangle = Rectangle()
 
     @Test
     fun `test converting a Rectangle to string`() {
         assertEquals(
-            "[5.0,-4.1,0.03,-0.02]",
-            Rectangle(5f, -4.1f, 0.03f, -0.02f).toString()
+            mockRectangleString,
+            mockRectangle.toString()
+        )
+        assertEquals(
+            "[0.0,0.0,0.0,0.0]",
+            defaultRectangle.toString()
         )
     }
 
     @Test
     fun `test converting string to Rectangle`() {
         assertEquals(
-            Rectangle(5f, -4.1f, 0.03f, -0.02f),
-            Rectangle().fromString("[5.0,-4.1,0.03,-0.02]")
+            mockRectangle,
+            Rectangle().fromString(mockRectangleString)
+        )
+        assertEquals(
+            defaultRectangle,
+            Rectangle().fromString("[0.0,0.0,0.0,0.0]")
+        )
+    }
+
+    @Test
+    fun `test area of Rectangle`() {
+        assertEquals(
+            (-5.9999997E-4).toFloat(), mockRectangle.area()
+        )
+        assertEquals(0.0f, defaultRectangle.area())
+    }
+
+    @Test
+    fun `test perimeter of Rectangle`() {
+        assertEquals(
+            0.02f, mockRectangle.perimeter()
+        )
+        assertEquals(
+            0.0f, defaultRectangle.perimeter()
         )
     }
 }
